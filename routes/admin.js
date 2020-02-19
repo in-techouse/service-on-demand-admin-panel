cc1f71876fa47156428cb515ba0f3adb32781449
Javeria Saeed <janeriasaeed102@gmail.com>
2020-02-19 15:13:47 +0500

    create empty pages

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,43 @@ router.get("/", function(req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
-  res.render("pages/admin/index");
+  res.render("pages/admin/index", {action: "dashboard" });
+});
+
+
+router.get("/createVendor", function(req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  res.render("pages/admin/createVendor", {action: "createVendor" });
+});
+
+router.get("/allVendor", function(req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  res.render("pages/admin/allVendor", {action: "allVendor" });
+});
+
+router.get("/allUser", function(req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  res.render("pages/admin/allUser", {action: "allUser" });
+});
+
+router.get("/allBooking", function(req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  res.render("pages/admin/allBooking", {action: "allBooking" });
+});
+
+router.get("/complains", function(req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  res.render("pages/admin/complains", {action: "complains" });
 });
 
 module.exports = router;
