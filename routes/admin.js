99fdc92c52197f6e8d83f88d7d3e0fd1fb1564d7
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-05-13 17:30:41 +0500

    Working on User Detail pahe

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require("express");
 var firebase = require("firebase");
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
@@ -10,14 +10,14 @@ router.get("/", function(req, res) {
     categories: 0,
     users: 0,
     vendors: 0,
-    bookings: 0
+    bookings: 0,
   };
   firebase
     .database()
     .ref()
     .child("Categories")
     .once("value")
-    .then(c => {
+    .then((c) => {
       count.categories = c.numChildren();
       firebase
         .database()
@@ -26,7 +26,7 @@ router.get("/", function(req, res) {
         .orderByChild("type")
         .equalTo(0)
         .once("value")
-        .then(u => {
+        .then((u) => {
           count.users = u.numChildren();
           firebase
             .database()
@@ -35,56 +35,52 @@ router.get("/", function(req, res) {
             .orderByChild("type")
             .equalTo(1)
             .once("value")
-            .then(v => {
+            .then((v) => {
               count.vendors = v.numChildren();
               firebase
                 .database()
                 .ref()
                 .child("Bookings")
                 .once("value")
-                .then(b => {
+                .then((b) => {
                   count.bookings = b.numChildren();
                   res.render("pages/admin/index", {
                     action: "dashboard",
                     session: req.session,
-                    count: count
+                    count: count,
                   });
                 });
             });
         });
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/index", {
         action: "dashboard",
         session: req.session,
-        count: count
+        count: count,
       });
     });
 });
 
-router.get("/createCategory", function(req, res) {
+router.get("/createCategory", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
   res.render("pages/admin/createCategory", {
     action: "createCategory",
-    session: req.session
+    session: req.session,
   });
 });
 
-router.post("/createCategory", function(req, res) {
+router.post("/createCategory", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
-  let id = firebase
-    .database()
-    .ref()
-    .child("Categories")
-    .push().key;
+  let id = firebase.database().ref().child("Categories").push().key;
   let category = {
     id: id,
     name: req.body.name,
-    url: req.body.url
+    url: req.body.url,
   };
   firebase
     .database()
@@ -92,18 +88,18 @@ router.post("/createCategory", function(req, res) {
     .child("Categories")
     .child(category.id)
     .set(category)
-    .then(r => {
+    .then((r) => {
       res.redirect("/admin/allCategories");
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/createCategory", {
         action: "createCategory",
-        session: req.session
+        session: req.session,
       });
     });
 });
 
-router.get("/allCategories", function(req, res) {
+router.get("/allCategories", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
@@ -112,23 +108,68 @@ router.get("/allCategories", function(req, res) {
     .ref()
     .child("Categories")
     .once("value")
-    .then(d => {
+    .then((d) => {
       res.render("pages/admin/allCategories", {
         action: "allCategories",
         session: req.session,
-        data: d
+        data: d,
       });
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/allCategories", {
         action: "allCategories",
         session: req.session,
-        data: []
+        data: [],
       });
     });
 });
 
-router.get("/createVendor", function(req, res) {
+router.get("/editCategory", function (req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  firebase
+    .database()
+    .ref()
+    .child("Categories")
+    .child(req.query.id)
+    .once("value")
+    .then((d) => {
+      res.render("pages/admin/editCategory", {
+        action: "editCategory",
+        session: req.session,
+        data: d.val(),
+      });
+    })
+    .catch((e) => {
+      res.redirect("/admmin/allCategories");
+    });
+});
+
+router.post("/editCategory", function (req, res) {
+  if (!req.session.isAdmin) {
+    res.redirect("/");
+  }
+  let category = {
+    id: req.body.id,
+    name: req.body.name,
+    url: req.body.url,
+  };
+  firebase
+    .database()
+    .ref()
+    .child("Categories")
+    .child(category.id)
+    .set(category)
+    .then((r) => {
+      res.redirect("/admin/allCategories");
+    })
+    .catch((e) => {
+      res.redirect("/admin/editCategory?id=" + req.body.id);
+    });
+});
+
+router.get("/createVendor", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
@@ -137,23 +178,23 @@ router.get("/createVendor", function(req, res) {
     .ref()
     .child("Categories")
     .once("value")
-    .then(c => {
+    .then((c) => {
       res.render("pages/admin/createVendor", {
         action: "createVendor",
         session: req.session,
-        data: c
+        data: c,
       });
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/createVendor", {
         action: "createVendor",
         session: req.session,
-        data: []
+        data: [],
       });
     });
 });
 
-router.post("/createVendor", function(req, res) {
+router.post("/createVendor", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
@@ -167,7 +208,7 @@ router.post("/createVendor", function(req, res) {
     perHourCharge: parseInt(req.body.perHourCharge),
     type: 1, // if type = 0 => Customer, and if type = 1 => Vendor
     image: "",
-    rating: 0
+    rating: 0,
   };
   firebase
     .database()
@@ -175,21 +216,21 @@ router.post("/createVendor", function(req, res) {
     .child("Users")
     .child(vendor.id)
     .set(vendor)
-    .then(r => {
+    .then((r) => {
       res.redirect("/admin/allVendor");
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/createVendor", {
         action: "createVendor",
-        session: req.session
+        session: req.session,
       });
     });
 });
 
-router.get("/allVendor", function(req, res) {
-  if (!req.session.isAdmin) {
-    res.redirect("/");
-  }
+router.get("/allVendor", function (req, res) {
+  // if (!req.session.isAdmin) {
+  //   res.redirect("/");
+  // }
   firebase
     .database()
     .ref()
@@ -197,23 +238,47 @@ router.get("/allVendor", function(req, res) {
     .orderByChild("type")
     .equalTo(1)
     .once("value")
-    .then(d => {
+    .then((d) => {
       res.render("pages/admin/allVendor", {
         action: "allVendor",
         session: req.session,
-        data: d
+        data: d,
       });
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/allVendor", {
         action: "allVendor",
         session: req.session,
-        data: []
+        data: [],
       });
     });
 });
 
-router.get("/allUser", function(req, res) {
+router.get("/vendorDetail", function (req, res) {
+  // if (!req.session.isAdmin) {
+  //   res.redirect("/");
+  // }
+  let id = req.query.id.replace(" ", "+");
+
+  firebase
+    .database()
+    .ref()
+    .child("Users")
+    .child(id)
+    .once("value")
+    .then((d) => {
+      res.render("pages/admin/vendorDetail", {
+        action: "vendorDetail",
+        session: req.session,
+        data: d.val(),
+      });
+    })
+    .catch((e) => {
+      res.redirect("/admin/allVendor");
+    });
+});
+
+router.get("/allUser", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
@@ -224,39 +289,39 @@ router.get("/allUser", function(req, res) {
     .orderByChild("type")
     .equalTo(0)
     .once("value")
-    .then(d => {
+    .then((d) => {
       res.render("pages/admin/allUser", {
         action: "allUser",
         session: req.session,
-        data: d
+        data: d,
       });
     })
-    .catch(e => {
+    .catch((e) => {
       res.render("pages/admin/allUser", {
         action: "allUser",
         session: req.session,
-        data: []
+        data: [],
       });
     });
 });
 
-router.get("/allBooking", function(req, res) {
+router.get("/allBooking", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
   res.render("pages/admin/allBooking", {
     action: "allBooking",
-    session: req.session
+    session: req.session,
   });
 });
 
-router.get("/complains", function(req, res) {
+router.get("/complains", function (req, res) {
   if (!req.session.isAdmin) {
     res.redirect("/");
   }
   res.render("pages/admin/complains", {
     action: "complains",
-    session: req.session
+    session: req.session,
   });
 });
 
